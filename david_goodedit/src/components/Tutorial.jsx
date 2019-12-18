d81aa767f638ac06c5e645cb8815c026bcd242ef
Maxime Thibaud <maximethibaudwcs@gmail.com>
2019-12-18 18:19:56 +0100

    I created a file css for the component Tutorial. I display the tuto for titles

--- david_goodedit/src/components/Tutorial.jsx
+++ david_goodedit/src/components/Tutorial.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import './Tutorial.css'
 
 function Tutorial() {
-  return <h1>Bonjour</h1>;
+  return (
+    <div>
+      <h1>Tutoriel</h1>
+      <div>
+        <h2>Les titres :</h2>
+        <div className="transformation">
+          <p>
+            #Titre 
+          </p>
+          <p className='titleH1'>
+            Titre
+          </p>
+        </div>
+        <div className="transformation">
+          <p>
+            ##Titre 
+          </p>
+          <p className='titleH2'>
+            Titre
+          </p>
+        </div>
+        <div className="transformation">
+          <p>
+            ###Titre 
+          </p>
+          <p className='titleH3'>
+            Titre
+          </p>
+        </div>
+        <div className="transformation">
+          <p>
+            ####Titre 
+          </p>
+          <p className='titleH4'>
+            Titre
+          </p>
+        </div>
+        <p>Plus vous rajouter de #, plus le titre est petit</p>
+      </div>
+    </div>
+  );
 }
 
 export default Tutorial
