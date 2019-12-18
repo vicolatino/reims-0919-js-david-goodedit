e770531b630d84e98479da2832b0898b345aeb08
Maxime Thibaud <maximethibaudwcs@gmail.com>
2019-12-18 18:52:39 +0100

    make an order list. not complete

--- david_goodedit/src/components/Tutorial.jsx
+++ david_goodedit/src/components/Tutorial.jsx
@@ -6,40 +6,36 @@ function Tutorial() {
     <div>
       <h1>Tutoriel</h1>
       <div>
-        <h2>Les titres :</h2>
-        <div className="transformation">
-          <p>
-            #Titre 
-          </p>
-          <p className='titleH1'>
-            Titre
-          </p>
+        <div>
+          <h2>Les titres :</h2>
+          <p>Plus vous rajouter de #, plus le titre est petit</p>
+          <div className="transformation">
+            <p>#Titre</p>
+            <p className='titleH1'>Titre</p>
+          </div>
+          <div className="transformation">
+            <p>##Titre</p>
+            <p className='titleH2'>Titre</p>
+          </div>
+          <div className="transformation">
+            <p>###Titre</p>
+            <p className='titleH3'>Titre</p>
+          </div>
+          <div className="transformation">
+            <p>####Titre</p>
+            <p className='titleH4'>Titre</p>
+          </div>
         </div>
-        <div className="transformation">
-          <p>
-            ##Titre 
-          </p>
-          <p className='titleH2'>
-            Titre
-          </p>
+        <div>
+          <h2>Les listes :</h2>
+          <h3>Les listes ordonnées :</h3>
+          <div className="transformation">
+            <p>1. Avoir une idée<br />2. Trouver des fonds<br />
+                3. ?<br />4. Profit
+            </p>
+            <p className='titleH4'>Titre</p>
+          </div>
         </div>
-        <div className="transformation">
-          <p>
-            ###Titre 
-          </p>
-          <p className='titleH3'>
-            Titre
-          </p>
-        </div>
-        <div className="transformation">
-          <p>
-            ####Titre 
-          </p>
-          <p className='titleH4'>
-            Titre
-          </p>
-        </div>
-        <p>Plus vous rajouter de #, plus le titre est petit</p>
       </div>
     </div>
   );
