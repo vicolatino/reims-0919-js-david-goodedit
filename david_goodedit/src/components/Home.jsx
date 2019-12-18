f2a60b231dd65f30af2d7aafd502b03e4d7bb65a
Victor Biard <vi.biard@gmail.com>
2019-12-18 16:54:12 +0100

    add Home.css

--- david_goodedit/src/components/Home.jsx
+++ david_goodedit/src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.css'
 
-function Home {
+function Home () {
     return (
       <div className="HomeContainer">
-        
+          <nav>
+              <a>START WRITTING<img src ='https://zupimages.net/up/19/51/08jy.png'/></a>
+          </nav>
       </div>
     );
   }
+
+export default Home
