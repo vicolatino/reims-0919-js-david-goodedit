3fbc82da12ee8d77dee9dda7b640ee3e08e08f48
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-18 17:22:41 +0100

    Add function to modify textarea's content in HTML

--- david_goodedit/src/components/InputArea.jsx
+++ david_goodedit/src/components/InputArea.jsx
@@ -4,13 +4,38 @@ import convertToHTML from 'markdown-to-html-converter'
 class InputArea extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      transformedText: ``
+    }
+    this.converter = this.converter.bind(this)
+  }
+  converter(event) {
+    this.setState({
+      transformedText: convertToHTML(event.target.value)
+    })
+    /*
+		this.setState(
+			(prevState, prevProps) => {
+				return {transformedText: convertToHTML(event.target.value)}
+			}
+		)
+		*/
   }
   render() {
     return (
       <div>
-        <label htmlFor=''>Markdown</label>
-        <textarea id='inputMD' name='inputMD'></textarea>
+        <div>
+          <label htmlFor=''>Markdown</label>
+          <textarea
+            id='inputMD'
+            name='inputMD'
+            onChange={this.converter}
+          ></textarea>
+        </div>
+        <div>
+          <h2>HTML</h2>
+          <div>{this.state.transformedText}</div>
+        </div>
       </div>
     )
   }
