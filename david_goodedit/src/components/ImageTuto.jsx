aa2c1f96dfdd10e5f3e067cea5d7d0ea967ea068
Maxime Thibaud <maximethibaudwcs@gmail.com>
2019-12-19 15:10:00 +0100

    css for UrlTuto, ImageTuto and ListsTuto

--- david_goodedit/src/components/ImageTuto.jsx
+++ david_goodedit/src/components/ImageTuto.jsx
@@ -3,14 +3,19 @@ import './Tutorial.css'
 
 function ImageTuto() {
   return (
-    <div>
-      <h2>Les images :</h2>
-        <p>![le titre de la photo entre crochet](le lien entre parenthèses après un point d'interrogation)</p>
+    <div className="boxImage">
+      <p>
+        <strong>Pour insérer une photo, on écrit de la manière suivante: </strong> 
+        <br />
+        ![le titre de la photo entre crochet](le lien entre parenthèses après un point d'interrogation)
+        </p>
+      <div className="exampleImage"> 
         <p>![David Goodenough :](https://pbs.twimg.com/profile_images/1002930959191298049/6U0HUeKu_400x400.jpg)</p>
         <div>
           <p>David Goodenough :</p>
           <img alt="David Goodenough" src="https://pbs.twimg.com/profile_images/1002930959191298049/6U0HUeKu_400x400.jpg" />
         </div>
+      </div>
     </div>
   );
 }
--- david_goodedit/src/components/UrlTuto.jsx
+++ david_goodedit/src/components/UrlTuto.jsx
@@ -3,14 +3,13 @@ import './Tutorial.css'
 
 function UrlTuto() {
   return (
-    <div>
-      <h2>Les URL :</h2>
-        <p>
-          Les URL peuvent être écrites de cette façon : 
-          [l'alias du site internet](l'URL du site)
-        <br />
-          ex : [Cat-bounce](https://cat-bounce.com/) => <a href="https://cat-bounce.com/">Cat-bounce</a>
-        </p>
+    <div className="boxUrl">
+      <p>
+        <strong>Les URL peuvent être écrites de cette façon </strong>  
+        => [l'alias du site internet](l'URL du site)
+      <br />
+        ex : [Cat-bounce](https://cat-bounce.com/) => <a href="https://cat-bounce.com/">Cat-bounce</a>
+      </p>
     </div>
   );
 }
