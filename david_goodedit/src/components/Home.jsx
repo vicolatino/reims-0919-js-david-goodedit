b40c901e65562e037fcfb2137c02c5cbb73ccdca
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-19 19:14:28 +0100

    Transform function Home into a class

--- david_goodedit/src/components/Home.jsx
+++ david_goodedit/src/components/Home.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import './Home.css'
 
-function Home () {
-    return (
-      <div className="HomeContainer flex-column">
-          <nav className="HomeNavbar flex-row perfect-center">
-              <a className="flex-row perfect-center">START WRITTING<img src ='https://zupimages.net/up/19/51/08jy.png'/></a>
-          </nav>
-          <div className='GoodEnoughContainer flex-column'>
-            <img src="https://zupimages.net/up/19/51/vn62.png" alt="" className="GoodEnough"/>
-          </div>
-          
-      </div>
-    );
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
   }
-
+  render() {
+    return (
+      <div className='HomeContainer flex-column'>
+        <nav className='HomeNavbar flex-row perfect-center'>
+          <a className='flex-row perfect-center'>
+            START WRITTING
+            <img src='https://zupimages.net/up/19/51/08jy.png' />
+          </a>
+        </nav>
+        <div className='GoodEnoughContainer flex-column'>
+          <img
+            src='https://zupimages.net/up/19/51/vn62.png'
+            alt=''
+            className='GoodEnough'
+          />
+        </div>
+      </div>
+    )
+  }
+}
 export default Home
