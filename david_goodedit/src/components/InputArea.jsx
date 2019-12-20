fc17258f06da0f9ea4b1d7481c5e8ac11d9de582
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-20 10:32:15 +0100

    ADD words counter

--- david_goodedit/src/components/InputArea.jsx
+++ david_goodedit/src/components/InputArea.jsx
@@ -18,6 +18,7 @@ class InputArea extends React.Component {
   converter(event) {
     this.setState({
       characters: event.target.value.length,
+      words: event.target.value.split(/[` ` | `'`]/g).length,
       transformedText: convertToHTML(event.target.value + '\n')
         .split(`\n`)
         .map(balise => {
