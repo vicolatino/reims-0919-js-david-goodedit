134b108cdd3e20e8c01a23b3ecb5369462675a80
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-20 12:32:19 +0100

    Merge with dev

--- david_goodedit/src/components/Home.jsx
+++ david_goodedit/src/components/Home.jsx
@@ -5,24 +5,35 @@ import { Link } from 'react-scroll'
 function Home({ aziz, light }) {
   return (
     <div className='HomeContainer flex-column'>
-      <button onClick={aziz}>{light === true ? 'Eteindre' : 'Allumer'}</button>
-          <nav className="HomeNavbar flex-row perfect-center">
-              <Link
-            activeClass='active'
-            className='flex-row perfect-center'
-            to='converterMk'
-            spy={true}
-            smooth={true}
-            offset={1}
-            duration={500}>
-            START WRITTING
-            <img src ='https://zupimages.net/up/19/51/08jy.png'/>
-          </Link>
-          </nav>
-          <div className='GoodEnoughContainer flex-column'>
-            <img src="https://zupimages.net/up/19/51/vn62.png" alt="" className="GoodEnough"/>
-          </div>
+      <nav className='HomeNavbar flex-row perfect-center'>
+        <Link
+          activeClass='active'
+          className='flex-row perfect-center'
+          to='converterMk'
+          spy={true}
+          smooth={true}
+          offset={1}
+          duration={500}
+        >
+          START WRITTING
+          <img src='https://zupimages.net/up/19/51/08jy.png' />
+        </Link>
+      </nav>
+      <div className='theme-switch-wrapper'>
+        <label class='theme-switch' for='checkbox'>
+          <input type='checkbox' id='checkbox' onChange={aziz} />
+
+          <div class='slider round'></div>
+        </label>
       </div>
+      <div className='GoodEnoughContainer flex-column'>
+        <img
+          src='https://zupimages.net/up/19/51/vn62.png'
+          alt=''
+          className='GoodEnough'
+        />
+      </div>
+    </div>
   )
 }
 
